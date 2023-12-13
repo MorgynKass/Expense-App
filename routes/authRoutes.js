63ff154f6c69386fb1384edc4a624fa5637ce545
MorgynKass <morgynkass@gmail.com>
2023-12-13 09:41:01 -0700

    finished backend

---
+++ routes/authRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import * as authController from "../controllers/authController.js";
+import requireAuth from "../middlewares/requireAuth.js";
+
+const router = express.Router();
+
+router.get("/all", authController.auth_all);
+router.get("/whoami", requireAuth, authController.auth_user);
+router.post("/auth", authController.auth_login);
+router.post("/logout", requireAuth, authController.auth_logout);
+router.post("/register", authController.auth_register);
+
+export default router;
